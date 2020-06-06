0a6c8860d589f9338ac6c54b3b003d2d536c8ca2
developerdiniz <mvl.rafael6@gmail.com>
2020-06-06 14:02:46 -0300

    Python basico - OO.

--- 01.Python basico - OO/Main.py
+++ 01.Python basico - OO/Main.py
@@ -1,4 +1,18 @@
 from veiculo import Veiculo
 
+
 caminhao_preto = Veiculo("Preto", 6 , "Volvo",80)
 print("Cor:",caminhao_preto.cor,", Rodas:",caminhao_preto.rodas,", Marca:",caminhao_preto.marca,", Tanque:",caminhao_preto.tanque)
+
+caminhao_preto.pintar('azul')
+print(f"Pintamos o caminha preto de {caminhao_preto.cor}")
+
+caminhao_preto.abastecer(20)
+
+caminhao_preto.viagem(90)
+
+
+
+
+
+
--- 01.Python basico - OO/veiculo.py
+++ 01.Python basico - OO/veiculo.py
@@ -1,9 +1,25 @@
 #criando uma classe.
 class Veiculo:
 
-    #criar uma metodo
+    #Criar um objeto veiculo
     def __init__(self,cor,rodas,marca,tanque):
         self.cor = cor
         self.rodas = rodas
         self.marca = marca
         self.tanque = tanque
+
+    #Metodos
+    def pintar(self,nova_cor):
+        self.cor = nova_cor
+        
+    #Metodos
+    def abastecer(self,litros):
+        self.tanque +=litros
+        print(f"Abastecemos {litros}, tanque com {self.tanque} litros")
+
+    #Metodos
+    def viagem(self, kilometros):
+        consumo = kilometros/9
+        self.tanque -= consumo
+        print(f"Tanque com {self.tanque} litros após a viagem de {kilometros} kilometros")
+        
