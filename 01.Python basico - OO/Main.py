3140cfd35c5d631f691b9214fa977ff6eefa9587
developerdiniz <mvl.rafael6@gmail.com>
2020-06-06 13:16:57 -0300

    Introdução a linguagem Python.

---
+++ 01.Python basico - OO/Main.py
@@ -0,0 +1,4 @@
+from veiculo import Veiculo
+
+caminhão_preto = Veiculo("preto",6,"volvo",'fiesta')
+print(caminhão_preto.modelo)
---
+++ 01.Python basico - OO/veiculo.py
@@ -0,0 +1,9 @@
+#criando uma classe.
+class Veiculo:
+
+    #criar uma metodo
+    def __init__(self,cor,rodas,marca, modelo):
+        self.cor = cor
+        self.rodas = rodas
+        self.marca = marca
+        self.modelo = modelo
