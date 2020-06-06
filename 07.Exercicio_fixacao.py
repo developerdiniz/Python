34ead9115d41b2991e85e5ccdb83648a3f28f92f
developerdiniz <mvl.rafael6@gmail.com>
2020-06-05 23:20:10 -0300

    Introdução a linguagem Python.

---
+++ 07.Exercicio_fixacao.py
@@ -0,0 +1,20 @@
+'''Exercicio:
+Escreva uma função que recebe um obejto de coleção e retorna o valor do maior numero dessa coleção e outra que recebe o menor'''
+ 
+lista = [2,59,635,48,1.5,7,8.75,0.75]
+
+#Criado duas funções diferentes para mostrar formas diferentes de logicas.
+#Usando um lado de repetição para percorrer a lista e comparar um por um para ver qual é o maior.
+def imprime_maior(lista):
+    x = 0
+    for y in lista:
+        if y > x :
+            x = y
+    return x
+
+#Ordenando a lista em ordem crescente e pegando o primeiro numero que por logica vai ser o menor
+#Para pegar o maior, poderiamos usar lista[-1]
+def imprime_menor(lista):
+    lista.sort()
+    return lista[0]
+
