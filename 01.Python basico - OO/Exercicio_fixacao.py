5bb2a0bf6c327103885b414346c48e41dd4110c8
developerdiniz <mvl.rafael6@gmail.com>
2020-06-07 00:35:40 -0300

    Python basico - OO.

---
+++ 01.Python basico - OO/Exercicio_fixacao.py
@@ -0,0 +1,5 @@
+'''Exercicio de fixação:
+Crie um software degerenciamento bancario
+Esse software poderá ser capaz de criar clientes e contas
+cada cliente possui nome, cpf, idade
+cada conta possui um cliente, saldo, limite, saca, depositar e consultar saldo'''
---
+++ 01.Python basico - OO/Resposta_exercicio_fixacao.py
@@ -0,0 +1,19 @@
+class conta:
+    def __init__(self,conta,nome,saldo,limite):
+        print("construindo objeto conta {}".format(self))
+        self.conta = conta
+        self.nome = nome
+        self.saldo = saldo
+        self.limite = limite
+        print(f"Conta:{conta}, Nome: {nome}, Saldo:{saldo}, Limite:{limite}")
+
+    def extrato(self):
+        return self.saldo
+    
+    def sacar(self, valor):
+        self.saldo -= valor
+        return self.extrato()
+
+    def depositar(self,valor):
+        self.saldo += valor
+        return self.extrato()
