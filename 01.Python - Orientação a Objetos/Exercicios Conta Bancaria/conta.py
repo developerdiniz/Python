7be385293a17aa99de926a7bf7e243d978a4047b
developerdiniz <mvl.rafael6@gmail.com>
2020-06-09 09:26:01 -0300

    Python - Orientação a objetos.

--- 01.Python - Orientação a Objetos/Exercicios Conta Bancaria/conta.py
+++ 01.Python - Orientação a Objetos/Exercicios Conta Bancaria/conta.py
@@ -1,19 +1,24 @@
 class CriarConta:
     def __init__(self,conta,titular,saldo,limite):
         print(f"construindo objeto conta {self}")
-        self.conta = conta
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__conta = conta
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
         print(f"Conta:{conta}, Titular: {titular}, Saldo:{saldo}, Limite:{limite}")
 
     def extrato(self):
-        return self.saldo
+        print(f"saldo na conta: {self.__saldo}")
     
     def sacar(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
         return self.extrato()
 
     def depositar(self,valor):
-        self.saldo += valor
+        self.__saldo += valor
         return self.extrato()
+
+    def transferir(self,destino,valor):
+        self.sacar(valor)
+        destino.depositar(valor)
+
---
+++ 01.Python - Orientação a Objetos/Exercicios Conta Bancaria/main.py
@@ -0,0 +1,9 @@
+from conta import CriarConta
+from cliente import Cliente
+
+conta_rafa = CriarConta(7153,"Rafael", 1040, 500)
+conta_nah = CriarConta(4868,"Natany",2000,500)
+
+conta_rafa.sacar(1050)
+
+
