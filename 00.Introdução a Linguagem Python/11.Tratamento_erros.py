8c9f7c202f7ebc57a3daa14db1bdc626f931c083
developerdiniz <mvl.rafael6@gmail.com>
2020-06-16 19:54:26 -0300

    Try en Except

--- 00.Introdução a Linguagem Python/11.Tratamento_erros.py
+++ 00.Introdução a Linguagem Python/11.Tratamento_erros.py
@@ -1,19 +1,15 @@
-'''lista1 = [1,2,3,1,4,9,5,0,7,2,6,7,8,9,0]
-lista2 = []
-
-for i in range(len(lista1)):
-    if lista1[i] in lista2:
-        print("tem")
-    else:
-        lista2.append(lista1[i])
-        print("nao")
-print(lista1,lista2)'''
-
-
-#tratar um erro 
-#não da para dividir um numero por zero, da erro. 
+#Tratar um erro 
+#Não da para dividir um numero por zero, da erro. 
 try:
     a = 1000 / 0
 except:
-    print("divisão por zero, não da pra fazer")
+    print("Divisão por zero, não da pra fazer")
     #Ao inves de dar erro, vai mostrar esse msg
+
+#Podemos tbm dar o except direto para o erro e ele vai tratar de acordo com o erro, ex:
+try: 
+   a= 100/0
+except ZeroDivisionError:
+    print("Divisão por zero, não da pra fazer")
+except NameError:
+    print("Você digitou alguma coisa errada")
