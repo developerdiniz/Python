e58df465583ee6d9083250c9bc801632b5b5fe43
developerdiniz <mvl.rafael6@gmail.com>
2020-08-31 08:57:36 -0300

    Requisições Web

--- 00.Introdução a Linguagem Python/11.Tratamento_erros.py
+++ 00.Introdução a Linguagem Python/11.Tratamento_erros.py
@@ -3,16 +3,16 @@
 try:
     a = 1000 / 0
 except:
-    print("Divis√£o por zero, n√£o da pra fazer")
+    print("Divis„o por zero, n„o da pra fazer")
     #Ao inves de dar erro, vai mostrar esse msg
 
 #Podemos tbm dar o except direto para o erro e ele vai tratar de acordo com o erro, ex:
 try: 
    a= 100/0
 except ZeroDivisionError:
-    print("Divis√£o por zero, n√£o da pra fazer")
+    print("Divis„o por zero, n„o da pra fazer")
 except NameError:
-    print("Voc√™ digitou alguma coisa errada")
+    print("VocÍ digitou alguma coisa errada")
 
 #Ou podemos deixar bem mais elaborado e dizer qual foi erro, atribuindo ele a uma variavel
 try:
---
+++ 00.Introdução a Linguagem Python/12.Requisicoes_web.py
@@ -0,0 +1,19 @@
+# Nome Procedimento: Aula - Bibliotecas, PIP e Requisições Web.
+# Descrição: Curso Solyd - Python básico 
+# Autor: Ricardo Couto Rafael Diniz
+# 31/Agosto/20
+# Versão: 1.0
+
+#Biblioteca para pegar requisições em paginas web
+import requests
+
+#Fazer um get na pagina do curso.
+requisicao = requests.get('https://solyd.com.br')
+#printar a requisição
+print(requisicao)
+
+#printar o status da requisição
+print(requisicao.status_code)
+
+#Ver o conteudo da pagina (Codigo fonte).
+print(requisicao.text)
